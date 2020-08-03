e1edd79f66a240fa31ca96a9d8971fff67824d41
mmenendez13 <matthewmenendez13@gmail.com>
2020-08-03 19:49:48 -0400

    fixed card spacing

--- client/src/components/cards/myItems.vue
+++ client/src/components/cards/myItems.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <b-row class="justify-content-md-center">
+        <b-row class="justify-content-md-center" style="position:relative; overflow-y:scroll">
             <b-card-group deck>
-                <b-col sm="12" lg="6" v-for="item in itemList" :key="item.clothingId">
-                    <b-card style="color:black">
+                <b-col sm="12" lg="12" v-for="item in itemList" :key="item.clothingId">
+                    <b-card style="color:black; height:100%;">
                         <b-card-text>{{item.description}}</b-card-text>
                     </b-card>
                 </b-col>
